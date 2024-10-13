c603c607f4a67391e1989e136d7863087e4ffbd0
chandan.singh@scaler.com <singhchandan374@gmail.com>
2024-10-14 00:17:14 +0530

    change: added form validations

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', {title: 'Code4Share - a platform for sharing code'})
+});
+
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', {title: 'Code4Share - a platform for sharing code'})
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Invalid Name').notEmpty();
+    req.checkBody('email', 'Invalid email').notEmpty();
+    req.checkBody('message', 'Invalid message').notEmpty();
+    var errors = req.validationErrors();
+    console.log(errors)
+    if (errors) {
+      res.render('contact', {
+        title: 'Code4share',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessage: errors
+      })
+    } else {
+      res.render('thank', {title: 'Code4Share - a platform for sharing code'}) 
+    }
+  });
+
 module.exports = router;
